0aef44b13bfc7f427047e22d828962c49e903f6c
Hazem Bayado <hbayado@gmail.com>
2015-02-17 18:44:19 +0400

    Solved Problems 1-6.

--- HBTableViewController.h
+++ HBTableViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  HBTableViewController.h
 //  UITableViewController Challenge
 //
-//  Created by Hazem Bayado on 2/16/15.
+//  Created by Hazem Bayado on 2/17/15.
 //  Copyright (c) 2015 Hawsaba. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HBTableViewController : UITableViewController
 
 @end
-
